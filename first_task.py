ac5b65725fdead64da5f989b45530a51a9db727b
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-09-07 19:26:42 +0300

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -1,16 +1,23 @@
 from random import randint
 
-first_massiv = []
-second_massiv = []
-result_massiv = []
+first_array = []
+second_array = []
+result_array = []
 
 for i in range(10):
-    first_massiv.append(randint(0,9))
-    second_massiv.append(randint(0, 9))
+    first_array.append(randint(0, 9))
+    second_array.append(randint(0, 9))
 
-for i in range(10):
-    result_massiv.append(first_massiv[i] + second_massiv[i])
+massive_for_reserved = 0
+for i in range(9, -1, -1):
+    add_result = first_array[i] + second_array[i] + massive_for_reserved
+    massive_for_reserved = add_result // 10
+    result_array.insert(0, add_result % 10)
 
-print(f"Перший масив: {first_massiv}")
-print(f"Другий масив: {second_massiv}")
-print(f"Результат: {result_massiv[::-1]}")
+if massive_for_reserved > 0:
+    result_array.insert(0, massive_for_reserved)
+
+# Вывод массивов и суммы в столбик
+print(f"Первый массив: {first_array}")
+print(f"Второй массив:{second_array}")
+print(f"Результат: {result_array}")
