63de10917d2fd82d6849c0187c842dfd2119cb5e
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-09-12 09:55:27 +0300

    Update first_task.py

--- first_task.py
+++ first_task.py
@@ -14,13 +14,14 @@ massive_for_reserved = 0
 
 for i in reversed(range(len_massiv)):
     add_result = first_array[i] + second_array[i] + massive_for_reserved
-    massive_for_reserved = add_result // 10
-    result_array.insert(0, add_result % 10)
+    if i == 0:
+        result_array.insert(0, add_result)
+    else:
+        massive_for_reserved = add_result // 10
+        result_array.insert(0, add_result % 10)
 
-if massive_for_reserved > 0:
-    result_array.insert(0, massive_for_reserved)
 
 
-print(f"Первый массив: {first_array}")
-print(f"Второй массив:{second_array}")
-print(f"Результат: {result_array}")
+print(f"Перший массив: {first_array}")
+print(f"Другий массив:{second_array}")
+print(f"\nРезультат: {result_array}")
